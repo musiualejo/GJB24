4bdb0f1f91a0b09be09320272ae63f55c3b10ae9
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-01-27 22:36:19 +0100

    Change timer color as it's running out

--- scripts/reloj.gd
+++ scripts/reloj.gd
@@ -10,6 +10,8 @@ class_name MinigameTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	progress_bar.tint_progress = Color(0.0, 1.0, 0.0)
+	progress_bar.max_value = duration
 	timer.start(duration)
 	timer.one_shot = false
 
@@ -17,6 +19,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	progress_bar.value = timer.time_left
+	if progress_bar.value <= progress_bar.max_value * 0.66 and progress_bar.value > progress_bar.max_value * 0.33:
+		progress_bar.tint_progress = Color(1.0, 1.0, 0.0)
+	elif progress_bar.value <= progress_bar.max_value * 0.33:
+		progress_bar.tint_progress = Color(1.0, 0.0, 0.0)
 
 
 func reset():
