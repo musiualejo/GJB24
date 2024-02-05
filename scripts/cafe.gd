f00f8cb35a38c27d7ce0982181928a1e38be109a
MusiuAlejo <157844124+musiualejo@users.noreply.github.com>
2024-02-05 03:22:17 +0100

    Animation tweaks + coffeecito

--- scripts/cafe.gd
+++ scripts/cafe.gd
@@ -4,6 +4,8 @@ var verbo := "keep them productive"
 var is_survival := true
 var started := false
 
+@export var puntaje: int = 10
+
 var tempo = 1
 var timer = 0
 
@@ -20,18 +22,22 @@ func _ready():
 func _process(delta):
 	if (timer == 0):
 		if Input.is_action_just_pressed("ui_down"):
+			Puntaje.emit(puntaje)
 			$personaje.frame = 1
 			$"coworkers/1/stamina".value = 100
 			timer = tempo
 		if Input.is_action_just_pressed("ui_right"):
+			Puntaje.emit(puntaje)
 			$personaje.frame = 2
 			$"coworkers/2/stamina".value = 100
 			timer = tempo
 		if Input.is_action_just_pressed("ui_left"):
+			Puntaje.emit(puntaje)
 			$personaje.frame = 3
 			$"coworkers/3/stamina".value = 100
 			timer = tempo
 		if Input.is_action_just_pressed("ui_up"):
+			Puntaje.emit(puntaje)
 			$personaje.frame = 4
 			$"coworkers/4/stamina".value = 100
 			timer = tempo
